d667fcc9bbad2585393686a887a046a0c262e4a8
ujju16 <jul.darrouza@gmail.com>
2022-08-30 22:34:38 +0200

    création kotlin/class DrawingView

--- app/src/main/java/com/example/billard/MainActivity.kt
+++ app/src/main/java/com/example/billard/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.billard
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawingView: DrawingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView = findViewById(R.id.vMain)
+    }
+
+    fun onClick(v: View) {
+        val x=10
+        if(x >= 10)
+            Toast.makeText(this, "Test du bouton", Toast.LENGTH_LONG).show()
     }
 }
